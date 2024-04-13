c279e0c215b18f35037329fc188621f0b9040f59
wojzam <wojciech.zamarski@student.pk.edu.pl>
2024-04-13 12:03:34 +0200

    Improve mysql import

--- import_mysql.py
+++ import_mysql.py
@@ -1,16 +1,27 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 from file_manager import *
+from mysql_tables import *
 
 
-def insert_table(engine, df, table_name):
-    with engine.begin() as conn:
-        df.to_sql(
-            name=table_name,
-            con=conn,
-            if_exists='replace',
-            index=False,
-        )
+def convert_value(value, dtype):
+    if pd.api.types.is_integer_dtype(dtype):
+        return int(value)
+    elif pd.api.types.is_float_dtype(dtype):
+        return float(value)
+    else:
+        return value
+
+
+def insert_df_to_table(df, table):
+    with Session() as session:
+        for _, row in df.iterrows():
+            instance_data = {col: convert_value(row[col], df[col]) for col in df.columns}
+            session.add(table(**instance_data))
+        session.commit()
+        print(f"Imported {table.__tablename__}")
 
 
 db_url = "mysql+mysqlconnector://{USER}:{PWD}@{HOST}/{DBNAME}"
@@ -21,14 +32,27 @@ db_url = db_url.format(
     DBNAME="boardgameDB"
 )
 
-engine = create_engine(db_url, echo=False)
+engine = create_engine(db_url)
+if not database_exists(engine.url):
+    create_database(engine.url)
 
-insert_table(engine, read_data("GAMES"), "GAMES")
-insert_table(engine, read_data("DEMAND"), "DEMAND")
-insert_table(engine, read_data("RATINGS"), "RATINGS")
-insert_table(engine, read_data('ARTISTS'), 'ARTISTS')
-insert_table(engine, read_data('DESIGNERS'), 'DESIGNERS')
-insert_table(engine, read_data('PUBLISHERS'), 'PUBLISHERS')
-insert_table(engine, read_data('THEMES'), 'THEMES')
-insert_table(engine, read_data('MECHANICS'), 'MECHANICS')
-insert_table(engine, read_data('SUBCATEGORIES'), 'SUBCATEGORIES')
+Base.metadata.create_all(engine, checkfirst=True)
+
+Session = sessionmaker(bind=engine)
+
+insert_df_to_table(read_data('GAMES'), Games)
+insert_df_to_table(read_data('DEMAND'), Demand)
+insert_df_to_table(read_data('RATINGS'), Ratings)
+
+insert_df_to_table(read_data('ARTISTS'), Artists)
+insert_df_to_table(read_data('DESIGNERS'), Designers)
+insert_df_to_table(read_data('PUBLISHERS'), Publishers)
+insert_df_to_table(read_data('THEMES'), Themes)
+insert_df_to_table(read_data('MECHANICS'), Mechanics)
+insert_df_to_table(read_data('SUBCATEGORIES'), Subcategories)
+
+insert_df_to_table(read_data('GAMES_ARTISTS'), GamesArtists)
+insert_df_to_table(read_data('GAMES_DESIGNERS'), GamesDesigners)
+insert_df_to_table(read_data('GAMES_PUBLISHERS'), GamesPublishers)
+insert_df_to_table(read_data('GAMES_MECHANICS'), GamesMechanics)
+insert_df_to_table(read_data('GAMES_SUBCATEGORIES'), GamesSubcategories)
---
+++ mysql_tables.py
@@ -0,0 +1,130 @@
+from sqlalchemy import Column, ForeignKey, Integer, Float, String, Text, Boolean
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Games(Base):
+    __tablename__ = 'games'
+    BGGId = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    Description = Column(Text)
+    YearPublished = Column(Integer)
+    MinPlayers = Column(Integer)
+    MaxPlayers = Column(Integer)
+    ComAgeRec = Column(Float)
+    LanguageEase = Column(Float)
+    MfgPlaytime = Column(Integer)
+    ComMinPlaytime = Column(Integer)
+    ComMaxPlaytime = Column(Integer)
+    MfgAgeRec = Column(Integer)
+    NumAlternates = Column(Integer)
+    NumExpansions = Column(Integer)
+    NumImplementations = Column(Integer)
+    IsReimplementation = Column(Boolean)
+
+    demand = relationship("Demand", uselist=False, backref="games")
+    ratings = relationship("Ratings", uselist=False, backref="games")
+    artists = relationship('Artists', secondary='games_artists', back_populates='games')
+    designers = relationship('Designers', secondary='games_designers', back_populates='games')
+    publishers = relationship('Publishers', secondary='games_publishers', back_populates='games')
+    themes = relationship('Themes', secondary='games_themes', back_populates='games')
+    subcategories = relationship('Subcategories', secondary='games_subcategories', back_populates='games')
+    mechanics = relationship('Mechanics', secondary='games_mechanics', back_populates='games')
+
+
+class Demand(Base):
+    __tablename__ = 'demand'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    NumOwned = Column(Integer)
+    NumWant = Column(Integer)
+    NumWish = Column(Integer)
+
+
+class Ratings(Base):
+    __tablename__ = 'ratings'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    GameWeight = Column(Integer)
+    AvgRating = Column(Float)
+    BayesAvgRating = Column(Float)
+    StdDev = Column(Float)
+    NumUserRatings = Column(Integer)
+    NumComments = Column(Integer)
+
+
+class Artists(Base):
+    __tablename__ = 'artists'
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    games = relationship('Games', secondary='games_artists', back_populates='artists')
+
+
+class Designers(Base):
+    __tablename__ = 'designers'
+    DesignerId = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    games = relationship('Games', secondary='games_designers', back_populates='designers')
+
+
+class Publishers(Base):
+    __tablename__ = 'publishers'
+    PublisherId = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    games = relationship('Games', secondary='games_publishers', back_populates='publishers')
+
+
+class Themes(Base):
+    __tablename__ = 'themes'
+    ThemeId = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    games = relationship('Games', secondary='games_themes', back_populates='themes')
+
+
+class Subcategories(Base):
+    __tablename__ = 'subcategories'
+    SubcategoryId = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    games = relationship('Games', secondary='games_subcategories', back_populates='subcategories')
+
+
+class Mechanics(Base):
+    __tablename__ = 'mechanics'
+    MechanicId = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    games = relationship('Games', secondary='games_mechanics', back_populates='mechanics')
+
+
+class GamesArtists(Base):
+    __tablename__ = 'games_artists'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    ArtistId = Column(Integer, ForeignKey('artists.ArtistId'), primary_key=True)
+
+
+class GamesDesigners(Base):
+    __tablename__ = 'games_designers'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    DesignerId = Column(Integer, ForeignKey('designers.DesignerId'), primary_key=True)
+
+
+class GamesPublishers(Base):
+    __tablename__ = 'games_publishers'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    PublisherId = Column(Integer, ForeignKey('publishers.PublisherId'), primary_key=True)
+
+
+class GamesThemes(Base):
+    __tablename__ = 'games_themes'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    ThemeId = Column(Integer, ForeignKey('themes.ThemeId'), primary_key=True)
+
+
+class GamesSubcategories(Base):
+    __tablename__ = 'games_subcategories'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    SubcategoryId = Column(Integer, ForeignKey('subcategories.SubcategoryId'), primary_key=True)
+
+
+class GamesMechanics(Base):
+    __tablename__ = 'games_mechanics'
+    BGGId = Column(Integer, ForeignKey('games.BGGId'), primary_key=True)
+    MechanicId = Column(Integer, ForeignKey('mechanics.MechanicId'), primary_key=True)
