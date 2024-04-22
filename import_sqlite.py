9ba8d0bae5f41e9c2661cac9604d82e3c88aca3f
wojzam <wojciech.zamarski@student.pk.edu.pl>
2024-04-22 14:28:30 +0200

    Remove unused code

--- import_sqlite.py
+++ import_sqlite.py
@@ -1,11 +1,7 @@
-import sqlite3
-
 from sqlalchemy import create_engine
 
 from sql_tables import *
 from utils_sql import insert_all_tables
-
-connection = sqlite3.connect("BoardGames.db")
 
 engine = create_engine('sqlite:///BoardGames.db', echo=True)
 
