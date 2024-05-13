61efe8960b3880884d4d043c0fc7000265107158
wojzam <wojciech.zamarski@student.pk.edu.pl>
2024-05-13 18:06:25 +0200

    Reformat code

--- queries.py
+++ queries.py
@@ -50,10 +50,11 @@ class Query(ABC):
     @abstractmethod
     def list_games_from_year_2020(self):
         pass
-    
+
     @abstractmethod
     def change_date_2020_to_2021(self):
         pass
+
 
 class SqlQuery(Query):
     def __init__(self, limit=DEFAULT_LIMIT):
@@ -74,9 +75,9 @@ class SqlQuery(Query):
 
     def list_games_names(self):
         return self.execute_query(lambda s: s.query(Games.Name))
-    
+
     def list_singleplayer_games_with_ratings(self):
-        return self.execute_query(lambda s: s.query(Games, Ratings).join(Ratings).where(Games.MinPlayers==1))
+        return self.execute_query(lambda s: s.query(Games, Ratings).join(Ratings).where(Games.MinPlayers == 1))
 
     def list_artists_names(self):
         return self.execute_query(lambda s: s.query(Artists.Name))
@@ -88,17 +89,17 @@ class SqlQuery(Query):
         return self.execute_query(
             lambda s: s.query(Games.Name, func.group_concat(Artists.Name).label('Artists')).select_from(Games).join(
                 GamesArtists).join(Artists).group_by(Games.Name))
-    
+
     def list_games_with_possible_zero_players(self):
-        return self.execute_query(lambda s: s.query(Games).where(Games.MinPlayers==0))
-    
-    #TODO
+        return self.execute_query(lambda s: s.query(Games).where(Games.MinPlayers == 0))
+
+    # TODO
     def delete_games_with_possible_zero_players(self):
         pass
 
     def list_games_from_year_2020(self):
         pass
-    
+
     def change_date_2020_to_2021(self):
         pass
 
@@ -134,33 +135,35 @@ class MongoDbQuery(Query):
 
     def list_games_names(self):
         return self.get_all('games', {}, {'Name': 1, '_id': 0})
-    
+
     def list_singleplayer_games_with_ratings(self):
-        return self.get_all('games', {'MinPlayers': 1}, {'Name': 1, 'Description': 1, 'YearPublished': 1, 'MinPlayers': 1, 'MaxPlayers': 1, 'Ratings': 1,'_id': 0})
-    
+        return self.get_all('games', {'MinPlayers': 1},
+                            {'Name': 1, 'Description': 1, 'YearPublished': 1, 'MinPlayers': 1, 'MaxPlayers': 1,
+                             'Ratings': 1, '_id': 0})
+
     def list_artists_names(self):
         return self.get_all('artists', {}, {'Name': 1, '_id': 0})
 
     def list_demand_with_game_name(self):
         return self.get_all('games', {}, {'Demand': 1, 'Name': 1, '_id': 0})
-    
+
     def list_games_with_possible_zero_players(self):
         return self.get_all('games', {'MinPlayers': 0})
-    
+
     def delete_games_with_possible_zero_players(self):
-        self.db['games'].delete_many({'MinPlayers': 0}) #TODO delete reference
+        self.db['games'].delete_many({'MinPlayers': 0})  # TODO delete reference
 
     def list_games_from_year_2020(self):
         return self.get_all('games', {'YearPublished': 2020})
-    
+
     def change_date_2020_to_2021(self):
-        self.db['games'].update_many({'YearPublished': 2020}, {"$set":{'YearPublished': 20}})
-    
+        self.db['games'].update_many({'YearPublished': 2020}, {"$set": {'YearPublished': 20}})
+
     def delete_games_with_possible_zero_players(self):
         self.db['games'].delete_many({'MinPlayers': 0})
-        #TODO delete reference
+        # TODO delete reference
 
-    #TODO
+    # TODO
     def list_game_artists(self):
         pass
 
@@ -189,9 +192,9 @@ class RedisQuery(Query):
 
     def list_games_names(self):
         return self.get_all('game:*', ['Name'])
-    
+
     def list_singleplayer_games_with_ratings(self):
-        return self.get_all('game:*', ['Name', 'Ratings']) #TODO
+        return self.get_all('game:*', ['Name', 'Ratings'])  # TODO
 
     def list_artists_names(self):
         return self.get_all('artist:*', ['Name'])
@@ -209,16 +212,16 @@ class RedisQuery(Query):
     def get_artist_names(self, ids):
         id_list = ids[1:-1].split(",")
         return [self.r.hget(f"artist:{int(float(x))}", "Name") if x != 'NaN' else "" for x in id_list]
-    
-    #TODO
+
+    # TODO
     def list_games_with_possible_zero_players(self):
         pass
-    
+
     def delete_games_with_possible_zero_players(self):
         pass
 
     def list_games_from_year_2020(self):
         pass
-    
+
     def change_date_2020_to_2021(self):
         pass
